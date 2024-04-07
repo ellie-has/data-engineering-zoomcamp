4b04e91847fc82e2e62adae1b02148a726ab0b78
ellie-has <38140393+ellie-has@users.noreply.github.com>
2024-04-07 17:45:18 +0300

    Update variables.tf

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
@@ -1,6 +1,6 @@
 variable "credentials" {
   description = "My Credentials"
-  default     = "/home/ellie/terraform_gc/feisty-album.json"
+  default     = "<Path to your Service Account json file>"
   #ex: if you have a directory where this file is called keys with your service account json file
   #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
@@ -8,31 +8,31 @@ variable "credentials" {
 
 variable "project" {
   description = "Project"
-  default     = "feisty-album-411415"
+  default     = "<Your Project ID>"
 }
 
 variable "region" {
   description = "Region"
   #Update the below to your desired region
-  default     = "europe-north1"
+  default     = "us-central1"
 }
 
 variable "location" {
   description = "Project Location"
   #Update the below to your desired location
-  default     = "EU"
+  default     = "US"
 }
 
 variable "bq_dataset_name" {
   description = "My BigQuery Dataset Name"
   #Update the below to what you want your dataset to be called
-  default     = "trips_data_all"
+  default     = "demo_dataset"
 }
 
 variable "gcs_bucket_name" {
-  description = "My Storage Bucket"
+  description = "My Storage Bucket Name"
   #Update the below to a unique bucket name
-  default     = "terraform-gcp-dezoom"
+  default     = "terraform-demo-terra-bucket"
 }
 
 variable "gcs_storage_class" {
